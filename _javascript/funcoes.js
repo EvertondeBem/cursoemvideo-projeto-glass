a84cc842bf25f96ea9ee8b3ca3299e5c9a375657
EvertondeBem <shelldmann@gmail.com>
2023-07-02 18:06:08 -0300

    curso em video, completo

---
+++ _javascript/funcoes.js
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+function mudaFoto (foto) {
+    document.getElementById("icone").src = foto;
+=======
+function mudaFoto (foto) {
+    document.getElementById("icone").src = foto;
+>>>>>>> b00437b864a22deb332d65308a544aa5495b1329
+}
